6f26bfed405071b81d9b2ed0134f2fe52d3b02f9
aashvi4946 <aashviswaroop1@gmail.com>
2023-11-18 00:43:15 +0530

    feat: todo list app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  // State Hook - `useState`
+
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
 
   const [showEdit, setShowEdit] = useState(-1);
   const [updatedText, setUpdatedText] = useState("");
 
-  // Helper Functions
-
-  /* Adds a new item to the list array*/
+  
   function addItem() {
-    // ! Check for empty item
+    
     if (!newItem) {
       alert("Press enter an item.");
       return;
@@ -24,25 +22,25 @@ function App() {
       value: newItem,
     };
 
-    // Add new item to items array
+    
     setItems((oldList) => [...oldList, item]);
 
-    // Reset newItem back to original state
+    
     setNewItem("");
   }
 
-  /* Deletes an item based on the `item.id` key */
+  
   function deleteItem(id) {
     const newArray = items.filter((item) => item.id !== id);
     setItems(newArray);
   }
 
-  /* Edit an item text after creating it. */
+  
   function editItem(id, newText) {
-    // Get the current item
+    
     const currentItem = items.filter((item) => item.id === id);
 
-    // Create a new item with same id
+    
     const newItem = {
       id: currentItem.id,
       value: newText,
@@ -50,13 +48,13 @@ function App() {
 
     deleteItem(id);
 
-    // Replace item in the item list
+    
     setItems((oldList) => [...oldList, newItem]);
     setUpdatedText("");
     setShowEdit(-1);
   }
 
-  // Main part of app
+  
   return (
     <div className="app">
       {/* 1. Header  */}
@@ -73,7 +71,7 @@ function App() {
       {/* Add (button) */}
       <button onClick={() => addItem()}>Add</button>
 
-      {/* 3. List of todos (unordered list) */}
+      {/* 3. List of todos  */}
       <ul>
         {items.map((item) => {
           return (
